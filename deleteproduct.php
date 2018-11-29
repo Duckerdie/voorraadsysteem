7a06d14443d3953fed4b722c2e824cd681e5a74f
Dustin <dgoldenlion@outlook.com>
2018-11-29 11:48:26 +0100

    Delete deleteproduct.php

--- deleteproduct.php
+++
@@ -1,9 +0,0 @@
-<?
-if (isset($_GET['id'])) {
-  $id = $_GET['id'];
-   $conn = mysqli_connect("localhost", "dustiqw271_usertest", "@z7gvbm8i@", "dustiqw271_test");
-   $deleteproduct = "DELETE FROM `products` WHERE `products`.`id` = ".$id.";";
-   $deleteproductcon = $conn->query($deleteproduct);
-   header('Location: index.php?message=Product with id '.$id.' has been deleted');
-}
-?>
